5cbe0f9efb5fceefb98478973d5fb633bfd10ecd
Deepak <Deepak@192.168.0.20>
2017-09-26 22:19:00 -0400

    change in c1.java class

--- src/p1/C1.java
+++ src/p1/C1.java
@@ -3,7 +3,7 @@ package p1;
 public class C1 {
 	public static void main(String[] args) {
 		System.out.println ("Selenium 1");// TODO Auto-generated method stub
-
+		System.out.println("Hello Git");
 	}
 
 
